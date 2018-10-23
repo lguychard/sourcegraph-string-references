4cc8014bfcc9787ab5b7b471c6f0fe2c42ccd030
Loïc Guychard <l.guychard@gmail.com>
2018-10-23 19:48:08 -0400

    fix: tests

--- src/extension.test.ts
+++ src/extension.test.ts
@@ -1,25 +1,126 @@
+import * as sinon from 'sinon'
+
+const SOURCEGRAPH = {
+    languages: {
+        registerHoverProvider: sinon.spy(),
+        registerReferenceProvider: sinon.spy(),
+    },
+    Position: sinon.spy(),
+    Range: sinon.spy(),
+    Uri: sinon.spy(),
+}
+
 const intercept = require('intercept-require')
 intercept(
     (moduleExport: {}, info: { moduleId: string }): {} => {
         if (info.moduleId === 'sourcegraph') {
-            return {
-                ...moduleExport,
-            }
+            return SOURCEGRAPH
         }
         return moduleExport
     }
 )
 
 import * as assert from 'assert'
-import { stringAtPosition } from './extension'
+import { stringAtPosition, activate } from './extension'
 
 describe('stringAtPosition', () => {
-    it('should find the hovered string', () => {
+    it('should find a single-quoted hovered string', () => {
+        const txt = ['the quick brown fox', "jumped 'over'", 'the lazy dog'].join('\n')
+        assert.deepEqual(
+            {
+                position: {
+                    end: 12,
+                    line: 1,
+                    start: 8,
+                },
+                value: 'over',
+            },
+            stringAtPosition(txt, {
+                line: 1,
+                character: 9,
+            } as any)
+        )
+    })
+
+    it('should find a double-quoted hovered string', () => {
         const txt = ['the quick brown fox', 'jumped "over"', 'the lazy dog'].join('\n')
-        const s = stringAtPosition(txt, {
-            line: 1,
-            character: 9,
-        } as any)
-        assert.equal('over', s && s.value)
+        assert.deepEqual(
+            {
+                position: {
+                    end: 12,
+                    line: 1,
+                    start: 8,
+                },
+                value: 'over',
+            },
+            stringAtPosition(txt, {
+                line: 1,
+                character: 9,
+            } as any)
+        )
+    })
+
+    it('should return null in case of mismatched quotes', () => {
+        const txt = ['the quick brown fox', `jumped "over'`, 'the lazy dog'].join('\n')
+        assert.strictEqual(
+            null,
+            stringAtPosition(txt, {
+                line: 1,
+                character: 9,
+            } as any)
+        )
+    })
+})
+
+describe('extension lifetime', () => {
+    let hoverProvider: Function | null = null
+    // let referenceProvider: Function | null = null
+
+    context('on activation', () => {
+        before(() => activate())
+
+        it('should register a hover provider', () => {
+            assert(SOURCEGRAPH.languages.registerHoverProvider.called)
+            const [selector, provider] = SOURCEGRAPH.languages.registerHoverProvider.getCalls()[0].args
+            assert.strictEqual(typeof provider.provideHover, 'function')
+            assert.deepStrictEqual(selector, ['*'])
+            hoverProvider = provider.provideHover
+        })
+
+        it('should register a reference provider', () => {
+            assert(SOURCEGRAPH.languages.registerReferenceProvider.called)
+            const [selector, provider] = SOURCEGRAPH.languages.registerReferenceProvider.getCalls()[0].args
+            assert.strictEqual(typeof provider.provideReferences, 'function')
+            assert.deepStrictEqual(selector, ['*'])
+            // referenceProvider = provider.provideReferences
+        })
+    })
+    context('on hover', () => {
+        it('should show a hover', () => {
+            if (!hoverProvider) {
+                throw new Error('hoverProvider not set')
+            }
+            const document = {
+                text: `the quick brown fox
+                jumped 'over'
+                the lazy dog`,
+            }
+            const position = {
+                line: 1,
+                character: 27,
+            }
+            const hover = hoverProvider(document, position)
+            assert.deepStrictEqual(hover, {
+                contents: {
+                    value: '**string literal** `"over"`',
+                    kind: 'markdown',
+                    range: {},
+                },
+            })
+        })
+    })
+    context('on find references request', () => {
+        it('should execute a graphQL query command')
+        it('should return references results based on the result of the command')
     })
 })
